1aec0f5bc9baeed4a89aae42de4f684a20f4a254
Даниил Костромитин <k36127@chuc.ru>
2023-02-15 14:01:23 +0500

    Вроде бы готово

--- Friday-13/Friday-13/Form1.Designer.cs
+++ Friday-13/Friday-13/Form1.Designer.cs
@@ -31,16 +31,19 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(383, 38);
+            this.button1.Location = new System.Drawing.Point(382, 38);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(171, 27);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Вывести все даты пятниц 13";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listBox1
             // 
@@ -57,16 +60,33 @@
             this.textBox1.Size = new System.Drawing.Size(170, 20);
             this.textBox1.TabIndex = 2;
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(383, 71);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(138, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(383, 97);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(138, 20);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(561, 195);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,6 +97,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
     }
 }
 
--- Friday-13/Friday-13/Form1.cs
+++ Friday-13/Friday-13/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace Friday_13
 {
@@ -15,6 +16,34 @@ namespace Friday_13
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cen, j, m;
+            m = 0;
+            j = 1;
+            string str = textBox1.Text;
+            cen = int.Parse(str);
+            Regex regex = new Regex(@"DayOfWeek == DayOfWeek.Friday(\w*)");
+            MatchCollection matches = regex.Matches(str);
+            for(int i = cen; i < cen + 1; i++)
+            {
+                for (j = 1; j <= 12; j++)
+                {
+                    DateTime dm = new DateTime(i, j, 13);
+                    if (dm.DayOfWeek == DayOfWeek.Friday)
+                        m += 1;
+                }
+            }
+            listBox1.Items.Add("Количество чёрных пятниц в " + str + " году" + " = " +
+            m.ToString());
         }
     }
 }
